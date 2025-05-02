4a0ddb31d2d724627fd1ea98f488c489c3b2cbfd
bryanlr96 <bryanlopezruiz1996@gmail.com>
2025-05-02 17:19:57 +0200

    comprobando la variable de entorno

--- src/context/ChatContext.jsx
+++ src/context/ChatContext.jsx
@@ -14,9 +14,9 @@ export const ChatProvider = ({ children }) => {
     useEffect(() => {
       const fetchUser = async () => {
         try {
-          console.log("------------------------------------------------------------")
+          console.log("-----------------------------------------------")
           console.log("variable de entorno url: " + process.env.REACT_APP_url_back)
-
+          console.log("-----------------------------------------------")
         
           const response = await fetch(`${process.env.REACT_APP_url_back}/reload`);
           if (!response.ok) throw new Error("Error al obtener usuario");
